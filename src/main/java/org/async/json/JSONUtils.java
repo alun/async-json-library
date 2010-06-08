b363d4a55ec4ff79e23d2315bbbda06fe66fc1bf
Mikhail Vorozhtsov <mikhail.vorozhtsov@gmail.com>
2010-06-08 16:03:38 +0700

    Commiting alun's patch.

--- src/main/java/org/async/json/JSONUtils.java
+++ src/main/java/org/async/json/JSONUtils.java
@@ -1,7 +1,10 @@
 package org.async.json;
 
+import java.io.IOException;
 import java.io.StringReader;
+import java.io.StringWriter;
 import org.async.json.in.JSONParser;
+import org.async.json.out.JSONWriter;
 
 /**
  *
@@ -9,8 +12,22 @@ import org.async.json.in.JSONParser;
  */
 public class JSONUtils {
 
-	public static JSONObject parseString( String string ) {
+	public static JSONObject parseObject( String string ) {
 		return new JSONParser().parse( new StringReader(string) );
+	}
+
+	public static JSONArray parseArray( String arrayString ) {
+		return new JSONParser().parseArray( new StringReader(arrayString) );
+	}
+
+	public static String toString( JSONObject jsonObject ) {
+		try {
+			StringWriter sw = new StringWriter();
+			new JSONWriter( sw ).writeObject( null, jsonObject );
+			return sw.toString();
+		} catch( IOException ioe ) {
+			throw  new RuntimeException(ioe);
+		}
 	}
 
 }
